1cd0db7abff3c268c0d019d5f18e4f6764ec13ff
dwloc <dwloch2@wp.pl>
2024-04-14 10:42:00 +0200

    addition of changes on styles.css and app.py-begining version

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import data_processing
 
 
 # Initialize the dash app
-app = dash.Dash(__name__, external_stylesheets=['styles.css'])
+app = dash.Dash(__name__, external_stylesheets=['./assets/styles.css'])
 
 # Fetching Titanic dataset
 titanic_data_url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
@@ -14,12 +14,12 @@ model = data_processing.train_model(X_train, y_train)
 
 # Define the layout of the app
 app.layout = html.Div([
-    html.H1("Titanic Survival Prediction"),
+    html.H1("Titanic Survival Prediction", className='header'),
 
     html.Div([
         html.Label("Select Age:"),
         dcc.Input(id='age-input', type='number')
-    ]),
+    ], className='component-div'),
 
     html.Div([
         html.Label("Select Gender:"),
@@ -27,7 +27,7 @@ app.layout = html.Div([
             {'label': 'Male', 'value': 0},
             {'label': 'Female', 'value': 1}
         ])
-    ]),
+    ],className='component-div'),
 
     html.Div([
         html.Label("Select Class:"),
@@ -36,21 +36,36 @@ app.layout = html.Div([
             {'label': 'Second', 'value': 2},
             {'label': 'Third', 'value': 3}
         ])
-    ]),
+    ],className='component-div'),
 
     html.Div([
         html.Label("Select Number of Siblings/Spouses Aboard:"),
-        dcc.Input(id='sibsp-input', type='number')
-    ]),
+        dcc.Slider(
+            id='sibsp-slider',
+            min=0,
+            max=10,
+            step=1,
+            value=0,
+            marks={i: str(i) for i in range(11)}  # Tworzenie etykiet dla suwaka
+        )
+    ],className='component-div re-slider-mark-text'),
 
     html.Div([
         html.Label("Select Number of Parents/Children Aboard:"),
-        dcc.Input(id='parch-input', type='number')
-    ]),
+        dcc.Slider(
+            id='parch-slider',
+            min=0,
+            max=10,
+            step=1,
+            value=0,
+            marks={i: str(i) for i in range(11)}  # Tworzenie etykiet dla suwaka
+        )
+    ],className='component-div re-slider-mark-text'),
 
-    html.Button('Predict', id='predict-button'),
-    html.Div(id='prediction-output')
-])
+    html.Button('Predict', id='predict-button', className='button-center'),
+    html.Br(),
+    html.Div(id='prediction-output', className='result-div',)
+], className='main-div')
 
 # Define the callback to predict survival
 @app.callback(
@@ -59,15 +74,18 @@ app.layout = html.Div([
     [Input('age-input', 'value'),
      Input('gender-dropdown', 'value'),
      Input('class-dropdown', 'value'),
-     Input('sibsp-input', 'value'),
-     Input('parch-input', 'value')]
+     Input('sibsp-slider', 'value'),  # Poprawiono identyfikator na 'sibsp-slider'
+     Input('parch-slider', 'value')]  # Poprawiono identyfikator na 'parch-slider'
 )
 def predict_survival(n_clicks, age, sex, pclass, sibsp, parch):
     if n_clicks is not None:
         data = [[pclass, sex, age, sibsp, parch, 0, 0]]  # Assuming Embarked and Fare are not used for prediction
         survival_prob = model.predict_proba(data)[0][1]  # Probability of belonging to class 'Survived'
-        return f"Survival Probability: {survival_prob:.2%}"
+        # Set text color based on survival probability
+        color = 'green' if survival_prob > 0.7 else 'red'
+        # Return styled text with color
+        return html.Span(f"Survival Probability: {survival_prob:.2%}", style={'color': color, 'font-weight': 'bold', 'font-size': '32px'})
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True, port=5002)
+    app.run_server(debug=True, port=5004)
