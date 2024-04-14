140e926bbb2011c4983f03342043b82a7b6a651d
dwloc <dwloch2@wp.pl>
2024-04-14 09:31:13 +0200

    create callback in app.py

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import dash
 from dash.dependencies import Input, Output
 import data_processing
 
-external_css = ["https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"]
 
 # Initialize the dash app
-app = dash.Dash(__name__, external_stylesheets=external_css)
+app = dash.Dash(__name__)
 
 # Fetching Titanic dataset
 titanic_data_url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
@@ -53,6 +52,21 @@ app.layout = html.Div([
     html.Div(id='prediction-output')
 ])
 
+# Define the callback to predict survival
+@app.callback(
+    Output('prediction-output', 'children'),
+    [Input('predict-button', 'n_clicks')],
+    [Input('age-input', 'value'),
+     Input('gender-dropdown', 'value'),
+     Input('class-dropdown', 'value'),
+     Input('sibsp-input', 'value'),
+     Input('parch-input', 'value')]
+)
+def predict_survival(n_clicks, age, sex, pclass, sibsp, parch):
+    if n_clicks is not None:
+        data = [[pclass, sex, age, sibsp, parch, 0, 0]]  # Assuming Embarked and Fare are not used for prediction
+        survival_prob = model.predict_proba(data)[0][1]  # Probability of belonging to class 'Survived'
+        return f"Survival Probability: {survival_prob:.2%}"
 
 # Run the app
 if __name__ == '__main__':
