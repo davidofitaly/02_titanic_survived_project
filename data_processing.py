987f82196d54408deab7e7ea6b47910126a98533
dwloc <dwloch2@wp.pl>
2024-04-14 08:28:10 +0200

    add function preprocess_data and train_model for model titanic

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,18 @@
 import pandas as pd
-from sklearn.esemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+
+def preprocess_data(titanic_data_url):
+    titanic_dif = pd.read_csv(titanic_data_url)
+    features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+    target = 'Survived'
+    titanic_df = titanic_df.dropna(subset=features + [target])
+    titanic_df['Sex'] = titanic_df['Sex'].map({'male': 0, 'female': 1})
+    titanic_df['Embarked'] = titanic_df['Embarked'].map({'C': 0, 'Q': 1, 'S': 2})
+    X_train, X_test, y_train, y_test = train_test_split(titanic_df[features], titanic_df[target], test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def train_model(X_train, y_train):
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+    return model
